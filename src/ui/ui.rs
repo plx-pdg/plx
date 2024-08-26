08a175b2f7016d571dd2beeb3fc0f991105e0f10
André Costa <toscmen.com@gmail.com>
2024-08-26 16:14:45 +0200

    refactor: add ui render fn

--- src/ui/ui.rs
+++ src/ui/ui.rs
@@ -1,44 +1,60 @@
-use std::sync::Mutex;
+use std::sync::{Mutex, Weak};
 
 use crate::{core::core::PlxCore, models::ui_state::UiState};
 
 pub struct Ui<'a> {
-    core: Mutex<PlxCore<'a>>,
+    core: Weak<Mutex<PlxCore<'a>>>,
 }
 impl Ui<'_> {
-    pub fn new(core: Mutex<PlxCore>) -> Ui<'_> {
+    pub fn new(core: Weak<Mutex<PlxCore>>) -> Ui<'_> {
         Ui { core }
     }
     fn setup(&mut self) {
         println!("Ui Setup...");
     }
+    fn teardown(&mut self) {
+        println!("Ui Teardown...");
+    }
     pub fn loop_forever(&mut self) {
         self.setup();
 
         loop {
-            if let Ok(core) = self.core.lock() {
-                match core.get_state() {
-                    UiState::StartMenu => println!("Starting..."),
-                    UiState::Quit => return,
-                    UiState::ChoosingSubject { subject_index } => todo!(),
-                    UiState::ChoosingExo {
-                        subject_index,
-                        exo_index,
-                    } => todo!(),
-                    UiState::ExoPromp {
-                        subject_index,
-                        exo_index,
-                        exo,
-                    } => todo!(),
-                    UiState::LoadingExo { exo } => todo!(),
-                    UiState::ExoLoaded { exo } => todo!(),
-                    UiState::Compiling { exo } => todo!(),
-                    UiState::CompileError { exo, error } => todo!(),
-                    UiState::DoingExo { exo, checks } => todo!(),
-                    UiState::ExoComplete { exo } => todo!(),
-                    UiState::ShowSolution { exo } => todo!(),
+            match self.core.upgrade() {
+                Some(core) => {
+                    if let Ok(core) = core.lock() {
+                        if !self.render(core.get_state()) {
+                            break;
+                        }
+                    }
                 }
+
+                None => break,
             }
         }
+        self.teardown();
+    }
+    fn render(&mut self, state: &UiState) -> bool {
+        match state {
+            UiState::StartMenu => println!("Starting..."),
+            UiState::Quit => return false,
+            UiState::ChoosingSubject { subject_index } => todo!(),
+            UiState::ChoosingExo {
+                subject_index,
+                exo_index,
+            } => todo!(),
+            UiState::ExoPromp {
+                subject_index,
+                exo_index,
+                exo,
+            } => todo!(),
+            UiState::LoadingExo { exo } => todo!(),
+            UiState::ExoLoaded { exo } => todo!(),
+            UiState::Compiling { exo } => todo!(),
+            UiState::CompileError { exo, error } => todo!(),
+            UiState::DoingExo { exo, checks } => todo!(),
+            UiState::ExoComplete { exo } => todo!(),
+            UiState::ShowSolution { exo } => todo!(),
+        }
+        return true;
     }
 }
