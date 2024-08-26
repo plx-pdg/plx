41833ee6c3be1dd2dc67c197761dc29d1bf71c98
André Costa <toscmen.com@gmail.com>
2024-08-26 16:26:04 +0200

    feat: add basic try from implementation to project

--- src/models/project.rs
+++ src/models/project.rs
@@ -1,6 +1,18 @@
+use crate::core::file_utils::file_parser;
+
 use super::skill::Skill;
 
 pub struct Project {
     name: String,
     skills: Vec<Skill>,
+}
+
+impl TryFrom<std::path::PathBuf> for Project {
+    type Error = file_parser::ParseError;
+
+    fn try_from(_path: std::path::PathBuf) -> Result<Self, Self::Error> {
+        // let content = read_file(path)?;
+        // parse_yaml(content)?;
+        Err(file_parser::ParseError::ParseError)
+    }
 }
