d221416a3a547c359edac52aaf0c1103293bd64d
kevin <kevin.auberson@heig-vd.ch>
2024-08-27 16:39:30 +0200

    feat: create watcher

--- src/core/work/work.rs
+++ src/core/work/work.rs
@@ -1,6 +1,8 @@
 use crate::core::editor::opener::EditorOpener;
+use crate::core::watcher::watcher::FileWatcher;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Work {
     EditorOpen(EditorOpener),
+    FileWatcher(FileWatcher)
 }
