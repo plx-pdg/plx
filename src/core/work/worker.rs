2e0418741b3588630b6c0af0bc61db6f6587d58c
André Costa <toscmen.com@gmail.com>
2024-08-27 10:25:15 +0200

    feat: avoid cloning by moving self to run. stop is handled elsewhere

--- src/core/work/worker.rs
+++ src/core/work/worker.rs
@@ -1,80 +1,42 @@
 use crate::models::event::Event;
 
-use super::work::Work;
+use super::{work::Work, work_handler::WorkEvent};
 use std::{
-    sync::{
-        atomic::{AtomicBool, Ordering},
-        mpsc::Sender,
-        Arc,
-    },
+    sync::{atomic::AtomicBool, mpsc::Sender, Arc},
     thread::{self, JoinHandle},
 };
 pub struct Worker {
+    id: usize,
+    work_tx: Sender<WorkEvent>,
     pub work_type: Work,
     pub tx: Sender<Event>,
     should_stop: Arc<AtomicBool>,
-    handle: Option<JoinHandle<()>>,
 }
 
 impl Worker {
-    pub fn new(tx: Sender<Event>, work_type: Work) -> Worker {
+    pub fn new(
+        id: usize,
+        work_tx: Sender<WorkEvent>,
+        tx: Sender<Event>,
+        should_stop: Arc<AtomicBool>,
+        work_type: Work,
+    ) -> Worker {
         Worker {
+            id,
+            work_tx,
             tx,
             work_type,
-            should_stop: Arc::new(AtomicBool::new(false)),
-            handle: None,
+            should_stop,
         }
     }
-    pub fn run(&mut self) {
-        match &self.work_type {
+    pub fn run(self) -> JoinHandle<()> {
+        match self.work_type {
             Work::EditorOpen(opener) => {
-                let opener = opener.clone();
-                let tx = self.tx.clone();
-                let should_stop = self.should_stop.clone();
-                self.handle = Some(thread::spawn(move || opener.run(tx, should_stop)));
+                return thread::spawn(move || {
+                    opener.run(self.tx, self.should_stop);
+                    self.work_tx.send(WorkEvent::Done(self.id));
+                });
             }
         }
-    }
-    pub fn stop(&mut self) {
-        self.should_stop.store(true, Ordering::Relaxed);
-        if let Some(handle) = self.handle.take() {
-            let _ = handle.join();
-        }
-    }
-    pub fn is_stop(&self) -> bool {
-        self.should_stop.load(Ordering::Relaxed)
-    }
-}
-#[cfg(test)]
-mod tests {
-    use std::sync::mpsc::channel;
-
-    use crate::core::editor::opener::EditorOpener;
-
-    use super::*;
-
-    #[test]
-    fn test_stop() {
-        let (tx, _) = channel();
-        let mut worker = Worker::new(
-            tx,
-            Work::EditorOpen(EditorOpener::new("echo".into(), "hello".into())),
-        );
-        assert_eq!(worker.should_stop.load(Ordering::Relaxed), false);
-        worker.stop();
-        assert_eq!(worker.should_stop.load(Ordering::Relaxed), true);
-    }
-    #[test]
-    fn test_handler() {
-        let (tx, _) = channel();
-        let mut worker = Worker::new(
-            tx,
-            Work::EditorOpen(EditorOpener::new("echo".into(), "hello".into())),
-        );
-        assert!(worker.handle.is_none());
-        worker.run();
-        assert!(worker.handle.is_some());
-        worker.stop();
-        assert!(worker.handle.is_none());
     }
 }
