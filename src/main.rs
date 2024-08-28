b6960d40b65350c327fef5ad0bab9381da938b1f
André Costa <toscmen.com@gmail.com>
2024-08-28 15:37:36 +0200

    refactor: remove dead code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
 pub mod core;
 pub mod models;
 pub mod ui;
-// fn main() {
-//     println!("PLX - Practice programming exos in a delightful Learning eXperience");
-//     println!("Testing CI");
-//     println!("Testing CI second time");
-// }
 use core::core::PlxCore;
 use ui::ui::Ui;
 fn main() -> io::Result<()> {
