54cb299355b9d04037cef11713c9db3b3a2f46f8
André Costa <toscmen.com@gmail.com>
2024-08-28 15:38:20 +0200

    refactor: new ui api

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,20 @@ pub mod core;
 pub mod models;
 pub mod ui;
 use core::core::PlxCore;
+use std::sync::{Arc, Mutex};
 use ui::ui::Ui;
-fn main() -> io::Result<()> {
+fn main() {
     let core = PlxCore::new();
-    let ui = Ui::new(core);
-    ui.loop_forever();
-    Ok(())
+    match core {
+        Some(core) => {
+            let mut ui = Ui::new(Arc::new(Mutex::new(core)));
+            match ui.loop_forever() {
+                Ok(_) => (),
+                Err(err) => eprintln!("{}", err),
+            }
+        }
+        None => {
+            eprintln!("Couldn't init Core");
+        }
+    }
 }
