caa46f9b121c15a21b9b7d058ba6a3a606c79bdf
GLK <114987519+G-LK@users.noreply.github.com>
2024-08-28 13:00:59 +0200

    feat: update UI

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 pub mod core;
 pub mod models;
 pub mod ui;
-fn main() {
-    println!("PLX - Practice programming exos in a delightful Learning eXperience");
-    println!("Testing CI");
-    println!("Testing CI second time");
+// fn main() {
+//     println!("PLX - Practice programming exos in a delightful Learning eXperience");
+//     println!("Testing CI");
+//     println!("Testing CI second time");
+// }
+use core::core::PlxCore;
+use ui::ui::Ui;
+fn main() -> io::Result<()> {
+    let core = PlxCore::new();
+    let ui = Ui::new(core);
+    ui.loop_forever();
+    Ok(())
 }
--- src/ui/ui.rs
+++ src/ui/ui.rs
@@ -1,6 +1,34 @@
-use std::sync::{Mutex, Weak};
+use std::{
+    io::Stdout,
+    sync::{Mutex, Weak},
+};
 
-use crate::{core::core::PlxCore, models::ui_state::UiState};
+use crate::{
+    core::core::PlxCore,
+    models::{event, ui_state::UiState},
+};
+use rand::Error;
+use ratatui::{
+    backend::CrosstermBackend,
+    crossterm::{
+        event::{self, Event, KeyCode},
+        terminal::{
+            self, disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
+        },
+        ExecutableCommand,
+    },
+    layout::{Constraint, Direction, Layout},
+    style::{Modifier, Style},
+    symbols::{block, line::BOTTOM_LEFT},
+    text::{Span, Text},
+    widgets::{Block, Borders, Paragraph},
+    DefaultTerminal, Frame, Terminal,
+};
+use std::{
+    io::{self, stdout},
+    ops::RangeBounds,
+    slice::Chunks,
+};
 
 pub struct Ui<'a> {
     core: Weak<Mutex<PlxCore<'a>>>,
@@ -9,31 +37,91 @@ impl Ui<'_> {
     pub fn new(core: Weak<Mutex<PlxCore>>) -> Ui<'_> {
         Ui { core }
     }
-    fn setup(&mut self) {
+    fn setup(&mut self)  -> io::Result<()> {
         println!("Ui Setup...");
+        enable_raw_mode()?;
+        stdout().execute(EnterAlternateScreen)?;
+        Ok(())
     }
-    fn teardown(&mut self) {
+    fn teardown(&mut self)  -> io::Result<()> {
         println!("Ui Teardown...");
+        disable_raw_mode()?;
+        stdout().execute(LeaveAlternateScreen)?;
+        Ok(())
     }
-    pub fn loop_forever(&mut self) {
-        self.setup();
+    pub fn loop_forever(&mut self) -> io::Result<()> {
+        self.setup()?;
+        let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
 
         loop {
             match self.core.upgrade() {
                 Some(core) => {
                     if let Ok(core) = core.lock() {
-                        if !self.render(core.get_state()) {
-                            break;
-                        }
+                        self.run(&mut terminal, core.get_state());
+                        // if !self.run(&mut terminal, core.get_state()) {
+                        //     break;
+                        // }
                     }
                 }
                 None => break,
             }
         }
-        self.teardown();
+        self.teardown()?;
+        Ok(())
     }
-    fn render(&mut self, state: &UiState) -> bool {
-        todo!();
+
+    pub fn run(
+        &mut self,
+        terminal: &mut Terminal<CrosstermBackend<Stdout>>,
+        state: &UiState,
+    ) -> Result<bool, io::Error> {
+        terminal.draw(|frame| {
+            if !self.render_frame(frame, state) {
+                // return Ok(false);
+            }
+        })?;
+        self.handle_events()?;
+        Ok(true)
+    }
+
+    fn render_frame(&self, frame: &mut Frame, state: &UiState) -> bool {
+        let display = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(1)
+            .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
+            .split(frame.size());
+
+        match state {
+            UiState::Home => {
+                let title = Paragraph::new(Text::from(Span::styled(
+                    "PLX",
+                    Style::default()
+                        .add_modifier(Modifier::BOLD)
+                        .add_modifier(Modifier::UNDERLINED),
+                )))
+                .block(Block::default().borders(Borders::ALL))
+                .alignment(ratatui::layout::Alignment::Center);
+
+                let content = Paragraph::new("Press 'r' to resume progress\nPress 'l' to list all Exercices\nPress '?' to display help\n").centered().block(Block::default().borders(Borders::ALL));
+
+                frame.render_widget(title, display[0]);
+                frame.render_widget(content, display[1]);
+            }
+            _ => {}
+        }
         return true;
     }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        todo!()
+    }
+
+    // fn handle_events(app_state: &mut AppState) -> io::Result<bool> {
+    //     if event::poll(std::time::Duration::from_millis(50))? {
+    //         if let Event::Key(key) = event::read()? {
+    //             //TODO send event to the core
+    //         }
+    //     }
+    //     Ok(false)
+    // }
 }
