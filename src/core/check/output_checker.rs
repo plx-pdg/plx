c8737c6807f01584624a8c94f18d53776a45377c
André <andre_miguel_costa@hotmail.com>
2024-08-31 00:50:58 +0200

    refactore: make output checker use the new check api

--- src/core/check/output_checker.rs
+++ src/core/check/output_checker.rs
@@ -2,7 +2,10 @@ use std::sync::{atomic::AtomicBool, mpsc::Sender, Arc};
 
 use crate::{
     core::diff::diff::Diff,
-    models::{check::Check, event::Event},
+    models::{
+        check::{Check, CheckTest},
+        event::Event,
+    },
 };
 
 #[derive(Debug)]
@@ -12,17 +15,17 @@ pub enum OutputCheckerCreationError {
 pub struct OutputChecker<'a> {
     id: usize,
     check: &'a Check,
-    program_output: &'a str,
+    program_output: String,
 }
 
 impl<'a> OutputChecker<'a> {
     pub fn new(
         id: usize,
         check: &'a Check,
-        program_output: &'a str,
+        program_output: String,
     ) -> Result<Self, OutputCheckerCreationError> {
-        match check {
-            Check::Output { .. } => Ok(Self {
+        match check.test {
+            CheckTest::Output { .. } => Ok(Self {
                 id,
                 check,
                 program_output,
@@ -31,12 +34,12 @@ impl<'a> OutputChecker<'a> {
         }
     }
     pub fn run(&self, tx: Sender<Event>, _should_stop: Arc<AtomicBool>) {
-        let expected = match self.check {
-            Check::Output { expected } => expected,
+        let expected = match &self.check.test {
+            CheckTest::Output { expected } => expected,
             //_ => return, // Will never get here
         };
 
-        let diff = Diff::calculate_difference(self.program_output, &expected, None);
+        let diff = Diff::calculate_difference(&self.program_output, expected, None);
 
         let event = if diff.contains_differences() {
             Event::OutputCheckFailed(self.id, diff)
@@ -58,20 +61,24 @@ mod test {
     fn test(check: Check, output: &str) -> Event {
         let (tx, rx) = channel();
 
-        let checker = OutputChecker::new(0, &check, output).unwrap();
+        let checker = OutputChecker::new(0, &check, output.to_string()).unwrap();
         checker.run(tx, Arc::new(AtomicBool::new(false)));
 
         rx.recv().unwrap()
     }
     #[test]
     fn test_id() {
-        let check = Check::Output {
-            expected: String::from("hello"),
+        let check = Check {
+            test: CheckTest::Output {
+                expected: String::from("hello"),
+            },
+            name: String::new(),
+            args: vec![],
         };
         let output = "hello";
         let (tx, rx) = channel();
 
-        let checker = OutputChecker::new(0, &check, output).unwrap();
+        let checker = OutputChecker::new(0, &check, output.to_string()).unwrap();
         checker.run(tx, Arc::new(AtomicBool::new(false)));
 
         let id = match rx.recv().unwrap() {
@@ -84,8 +91,12 @@ mod test {
     }
     #[test]
     fn test_same_output() {
-        let check = Check::Output {
-            expected: String::from("hello"),
+        let check = Check {
+            test: CheckTest::Output {
+                expected: String::from("hello"),
+            },
+            name: String::new(),
+            args: vec![],
         };
         let output = "hello";
         let event = test(check, output);
@@ -94,8 +105,12 @@ mod test {
 
     #[test]
     fn test_extra_word_at_the_end_fails() {
-        let check = Check::Output {
-            expected: String::from("hello"),
+        let check = Check {
+            test: CheckTest::Output {
+                expected: String::from("hello"),
+            },
+            name: String::new(),
+            args: vec![],
         };
         let output = "hello world";
 
@@ -105,8 +120,12 @@ mod test {
 
     #[test]
     fn test_extra_whitespace_at_the_end_passes() {
-        let check = Check::Output {
-            expected: String::from("hello"),
+        let check = Check {
+            test: CheckTest::Output {
+                expected: String::from("hello"),
+            },
+            name: String::new(),
+            args: vec![],
         };
         let output = "hello ";
 
@@ -116,8 +135,12 @@ mod test {
 
     #[test]
     fn test_extra_whitespace_beggining_fails() {
-        let check = Check::Output {
-            expected: String::from("hey\nhello"),
+        let check = Check {
+            test: CheckTest::Output {
+                expected: String::from("hey\nhello"),
+            },
+            name: String::new(),
+            args: vec![],
         };
         let output = "hey\n hello";
         let event = test(check, output);
@@ -126,8 +149,12 @@ mod test {
 
     #[test]
     fn test_tab_at_the_end_passes() {
-        let check = Check::Output {
-            expected: String::from("yoo\nhello"),
+        let check = Check {
+            test: CheckTest::Output {
+                expected: String::from("yoo\nhello"),
+            },
+            name: String::new(),
+            args: vec![],
         };
         let output = "yoo\t\nhello\t";
 
@@ -137,8 +164,12 @@ mod test {
 
     #[test]
     fn test_mix_of_whitespaces_at_end_passes() {
-        let check = Check::Output {
-            expected: String::from("yoo     \t\t  \nhello\t \n"),
+        let check = Check {
+            test: CheckTest::Output {
+                expected: String::from("yoo     \t\t  \nhello\t \n"),
+            },
+            name: String::new(),
+            args: vec![],
         };
         let output = "yoo\t\nhello\t";
 
@@ -147,8 +178,12 @@ mod test {
     }
     #[test]
     fn test_new_line_at_the_end_is_passes() {
-        let check = Check::Output {
-            expected: String::from("hey there\nhello\n"),
+        let check = Check {
+            test: CheckTest::Output {
+                expected: String::from("hey there\nhello\n"),
+            },
+            name: String::new(),
+            args: vec![],
         };
         let output = "hey there\nhello";
 
@@ -158,8 +193,12 @@ mod test {
 
     #[test]
     fn test_new_word_at_beginning_fails() {
-        let check = Check::Output {
-            expected: String::from("hello"),
+        let check = Check {
+            test: CheckTest::Output {
+                expected: String::from("hello"),
+            },
+            name: String::new(),
+            args: vec![],
         };
         let output = "world hello";
 
@@ -169,8 +208,12 @@ mod test {
 
     #[test]
     fn test_extra_lines_fails() {
-        let check = Check::Output {
-            expected: String::from("hello\n\nworld"),
+        let check = Check {
+            test: CheckTest::Output {
+                expected: String::from("hello\n\nworld"),
+            },
+            name: String::new(),
+            args: vec![],
         };
         let output = "hello\nworld";
 
