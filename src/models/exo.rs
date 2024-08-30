b6d6d08327f458cbdcabdd810b9b1f1cfa51b3b5
André <andre_miguel_costa@hotmail.com>
2024-08-30 15:01:45 +0200

    feat: add find_exo_files fn

--- src/models/exo.rs
+++ src/models/exo.rs
@@ -1,3 +1,4 @@
+use std::fs::ReadDir;
 use serde::{Deserialize, Serialize};
 use super::{check::Check, exo_state::ExoState, solution::Solution};
 
@@ -35,5 +36,38 @@ impl Exo {
             Some(file) => Some(file),
             None => self.files.first(),
         }
+    }
+}
+impl Exo {
+    fn find_exo_files(
+        dir_entries: ReadDir,
+    ) -> (Vec<std::path::PathBuf>, Option<std::path::PathBuf>) {
+        let mut exo_files = Vec::new();
+        let mut solution_file = None;
+        for entry in dir_entries {
+            if let Ok(entry) = entry {
+                let file_path = entry.path();
+                let file_path_str = file_path.display().to_string();
+                let file_extension = file_path
+                    .extension()
+                    .and_then(|extension| extension.to_str())
+                    .or(Some(""))
+                    .unwrap();
+
+                if file_path_str.contains(".sol.") {
+                    // TODO handle the case where we have multiple solutions ?
+                    solution_file = Some(file_path);
+                    continue;
+                }
+                if file_extension == "toml" {
+                    continue;
+                }
+                // TODO maybe make sure we don't mix .c with .java files here ?
+                // We need to be careful adding this because .c can be mixed with .cpp, .h,
+                // .hpp etc...
+                exo_files.push(file_path);
+            }
+        }
+        (exo_files, solution_file)
     }
 }
