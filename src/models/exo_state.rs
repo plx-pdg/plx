31a924490376a6b44a56534d25f44d24c10fc85d
André <andre_miguel_costa@hotmail.com>
2024-08-30 14:57:26 +0200

    feat: add exostate from_file fn

--- src/models/exo_state.rs
+++ src/models/exo_state.rs
@@ -1,7 +1,24 @@
 use serde::{Deserialize, Serialize};
+
+use crate::core::file_utils::{file_parser::ParseError, file_utils::read_file};
+
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
 pub enum ExoState {
     Todo,       // all checks are failing
     InProgress, // at least one successful check but not all of them
     Done,       // all checks are successful
+}
+
+impl ExoState {
+    pub fn from_file(file_path: std::path::PathBuf) -> Self {
+        if let Ok(file_content) = read_file(&file_path) {
+            if file_content.contains("done") {
+                ExoState::Done
+            } else {
+                ExoState::InProgress
+            }
+        } else {
+            ExoState::Todo
+        }
+    }
 }
