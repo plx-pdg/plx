ca4f0df02f07f2a818dffcd1dc7802426a332937
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2024-08-30 22:19:40 +0200

    fix: fix and improve PlxProjNotFound error message with `course.toml`

--- src/core/core_error.rs
+++ src/core/core_error.rs
@@ -8,9 +8,9 @@ pub enum CoreInitError {
 impl fmt::Display for CoreInitError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            CoreInitError::PlxProjNotFound => write!(f, ".plxproj folder not found"),
+            CoreInitError::PlxProjNotFound => write!(f, "PLX didn't find a course.toml in the current folder, this is not the root of a PLX course."),
             CoreInitError::ProjFilesParsingError(detail) => {
-                write!(f, "Parsing error {}", detail)
+                write!(f, "Parsing error: {}", detail)
             }
         }
     }
