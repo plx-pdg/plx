9cbe7fa8ee7aaa4d03c870e43f8055f7834505fe
André <andre_miguel_costa@hotmail.com>
2024-08-30 12:02:46 +0200

    feat: add parser mod

--- src/core.rs
+++ src/core.rs
@@ -2,5 +2,6 @@ pub mod app;
 pub mod diff;
 pub mod editor;
 pub mod file_utils;
+pub mod parser;
 pub mod process;
 pub mod work;
---
+++ src/core/parser/toml_parser.rs
@@ -0,0 +1,4 @@
+pub fn read_from_toml<T>(txt: &str) -> Result<(), toml::de::Error> {
+    // toml::from_str::<T>(txt)
+    Ok(())
+}
