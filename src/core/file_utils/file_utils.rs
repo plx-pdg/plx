dafa250015e0695f66b0915f5c20d7a13d068ca8
André <andre_miguel_costa@hotmail.com>
2024-08-30 15:15:01 +0200

    feat: add list_dir_folders and list_dir_files util fns

--- src/core/file_utils/file_utils.rs
+++ src/core/file_utils/file_utils.rs
@@ -6,3 +6,20 @@ pub fn read_file(file_path: &std::path::PathBuf) -> Result<String, io::Error> {
 pub fn list_dir(directory: &std::path::PathBuf) -> Result<ReadDir, io::Error> {
     std::fs::read_dir(directory)
 }
+
+pub fn list_dir_folders(dir: &std::path::PathBuf) -> Result<Vec<std::path::PathBuf>, io::Error> {
+    Ok(std::fs::read_dir(dir)?
+        .into_iter()
+        .filter(|r| r.is_ok()) // Get rid of Err variants for Result<DirEntry>
+        .map(|r| r.unwrap().path()) // This is safe, since we only have the Ok variants
+        .filter(|r| r.is_dir()) // Filter out non-folders
+        .collect())
+}
+pub fn list_dir_files(dir: &std::path::PathBuf) -> Result<Vec<std::path::PathBuf>, io::Error> {
+    Ok(std::fs::read_dir(dir)?
+        .into_iter()
+        .filter(|r| r.is_ok()) // Get rid of Err variants for Result<DirEntry>
+        .map(|r| r.unwrap().path()) // This is safe, since we only have the Ok variants
+        .filter(|r| r.is_file()) // Filter out non-folders
+        .collect())
+}
