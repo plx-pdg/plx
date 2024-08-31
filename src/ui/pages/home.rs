4b63b8445a2eba4435848068b82e3aad76bef66f
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2024-08-31 15:31:16 +0200

    feat: add gradient slogan, version number, website, repos, and remove constants value in home but import them from ui::utils

--- src/ui/pages/home.rs
+++ src/ui/pages/home.rs
@@ -1,78 +1,59 @@
+use crate::ui::utils::{
+    center, get_gradient_line, ASCII_LOGO, LOGO_LEFT, LOGO_RIGHT, LOGO_RIGHT_RGB, SLOGAN,
+};
 use ratatui::{
-    layout::{Constraint, Flex, Layout, Rect},
-    style::{Color, Style},
+    layout::Constraint,
+    style::Stylize,
     text::{Line, Span, Text},
-    widgets::{Block, Borders, Paragraph},
+    widgets::Paragraph,
     Frame,
 };
 
-// Function to mix colors and create the gradient result
-// factor = float qui indique la pos entre la couleur de first et second.
-// on fait les diff entre les r, g, b des 2 couleurs en utilisant le factor.
-fn mixed_color(start: (u8, u8, u8), end: (u8, u8, u8), factor: f32) -> (u8, u8, u8) {
-    let r = start.0 as f32 + factor * (end.0 as f32 - start.0 as f32);
-    let g = start.1 as f32 + factor * (end.1 as f32 - start.1 as f32);
-    let b = start.2 as f32 + factor * (end.2 as f32 - start.2 as f32);
-    (r as u8, g as u8, b as u8)
-}
-
-// From ratatui recipe https://ratatui.rs/recipes/layout/center-a-rect/
-pub fn center(area: Rect, horizontal: Constraint, vertical: Constraint) -> Rect {
-    let [area] = Layout::horizontal([horizontal])
-        .flex(Flex::Center)
-        .areas(area);
-    let [area] = Layout::vertical([vertical]).flex(Flex::Center).areas(area);
-    area
-}
-
 pub fn render_home(frame: &mut Frame) {
-    let ascii_logo = r#"
-████████  ██       ██     ██
-██     ██ ██        ██   ██ 
-██     ██ ██         ██ ██  
-████████  ██          ███   
-██        ██         ██ ██  
-██        ██        ██   ██ 
-██        ████████ ██     ██"#;
-    let quick_help_lines = vec![
-        "Press 'r' to resume progress",
-        "Press 'l' to list all exercices",
-        "Press '?' to display help",
-    ];
-
-    // Generate a gradient manually on ascii_logo from left to right
-    // Ratatui needs RGB => so we need convert hex colors to RGB
-    let left_color = (252, 17, 0); // #fc1100 => RGB = 252, 17, 0
-    let right_color = (255, 176, 0); // #ffb000 => RGB = 255, 176, 0
-
-    let gradient_width = ascii_logo.lines().map(str::len).max().unwrap_or_default();
-    // Split the text in lines and columns to distribute the color of the gradiant
-    // Generate a new line per existing line, including a colored char per existing char
-    // The color of char is defined via mixed_color() with factor from 0 to 1
-    let lines_from_text: Vec<&str> = ascii_logo.lines().collect();
+    // Generate a gradient manually on ASCII_LOGO from left to right
+    // Split the text in lines and columns to distribute the color of the gradient
+    let gradient_width = ASCII_LOGO.lines().map(str::len).max().unwrap_or_default();
     let mut lines: Vec<Line> = vec![];
-    for line in lines_from_text.iter() {
-        let mut new_line = Line::default();
-        for (j, c) in line.chars().enumerate() {
-            //TODO: fix this hacky -30 used to have the gradient approximatively reach right yellow
-            let factor = j as f32 / (gradient_width as f32 - 30 as f32) as f32;
-            let color = mixed_color(left_color, right_color, factor);
-            let style = Style::default().fg(Color::Rgb(color.0, color.1, color.2));
-            new_line.push_span(Span::styled(c.to_string(), style));
-        }
-        lines.push(new_line.clone());
+    for line in ASCII_LOGO.lines() {
+        lines.push(get_gradient_line(
+            line,
+            LOGO_LEFT,
+            LOGO_RIGHT,
+            //TODO: fix this hacky -30 to have the gradient approximatively reach right yellow
+            gradient_width as f32 - 30 as f32,
+        ));
     }
+
+    // Append slogan + version line
+    let mut slogan_line = get_gradient_line(SLOGAN, LOGO_LEFT, LOGO_RIGHT, SLOGAN.len() as f32);
+    lines.push(Line::default()); //margin top
+    slogan_line.push_span(Span::from(" - ").dim());
+    let version = Span::from(format!("v{}", env!("CARGO_PKG_VERSION"))).fg(LOGO_RIGHT_RGB);
+    slogan_line.push_span(version);
+    lines.push(slogan_line);
+
+    // Append website and repository
+    lines.push(Line::from("Repository: https://github.com/plx-pdg/plx").dim());
+    lines.push(Line::from("Website: https://plx.rs").dim());
+
     // Append quick help
-    lines.push(Line::from("")); //margin top for quick help
+    let quick_help_lines = vec![
+        "Type r to resume progress",
+        "Type l to list all exos",
+        "Type ? to display help",
+        "Type q to quit",
+    ];
+    lines.push(Line::default()); //margin top
+    lines.push(Line::from("Quick help").bold()); //margin top for quick help
     for l in quick_help_lines {
-        lines.push(Line::from(l));
+        lines.push(Line::from(l).dim());
     }
 
     // The global zone is centered horizontally and vertically !
     // Helped by https://ratatui.rs/recipes/layout/center-a-rect/
     let width = lines.iter().map(|l| l.width()).max().unwrap_or_default();
     let height = lines.len();
-    let zone = Paragraph::new(Text::from(lines));
+    let zone = Paragraph::new(Text::from(lines)).centered();
     let centered_area = center(
         frame.area(),
         Constraint::Length(width as u16),
