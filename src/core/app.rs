150e5f806dea207882b92828b3910b3006691d20
André <andre_miguel_costa@hotmail.com>
2024-09-02 16:10:28 +0200

    feat: init ui after initializing app

--- src/core/app.rs
+++ src/core/app.rs
@@ -3,21 +3,26 @@ use std::sync::{
     Arc, Mutex,
 };
 
-use crate::models::{event::Event, exo::Exo, project::Project, ui_state::UiState};
+use crate::{
+    models::{event::Event, exo::Exo, project::Project, ui_state::UiState},
+    ui::ui::Ui,
+};
 
 use super::{
     core_error::CoreInitError,
     editor::opener::EditorOpener,
     file_utils::file_handler::current_folder,
     parser::from_dir::FromDir,
-    work::{work::Work, work_handler::WorkHandler},
+    work::{work::Work, work_handler::WorkHandler, work_type::WorkType},
 };
 
 pub struct App {
     ui_state: UiState,
     project: Project,
     work_handler: Arc<Mutex<WorkHandler>>,
-    event_queue: (Sender<Event>, Receiver<Event>),
+    event_tx: Sender<Event>,
+    event_rx: Receiver<Event>,
+    ui_state_tx: Sender<UiState>,
 }
 
 impl App {
@@ -27,7 +32,6 @@ impl App {
             Err(_err) => return Err(CoreInitError::PlxProjNotFound), // TODO maybe be more specific
                                                                      // here by adding the error detail
         };
-
         // TODO these warnings should be accessible to the user
         let (project, _warnings) = match Project::from_dir(&current_folder) {
             Ok((project, warnings)) => (project, warnings),
@@ -36,18 +40,22 @@ impl App {
                 return Err(CoreInitError::ProjFilesParsingError(format!("{:?}", err)));
             }
         };
+        let (event_tx, event_rx) = mpsc::channel();
+        let (ui_state_tx, ui_state_rx) = mpsc::channel();
+        let work_handler = WorkHandler::new(event_tx.clone());
 
-        let channel = mpsc::channel();
-        Ok(App {
+        let mut app = App {
             ui_state: UiState::Home,
             project,
-            work_handler: (WorkHandler::new(channel.0.clone())),
-            event_queue: channel,
-        })
+            work_handler,
+            event_tx,
+            event_rx,
+            ui_state_tx,
+        };
+        app.start_ui(ui_state_rx);
+        Ok(app)
     }
-    pub fn get_state(&self) -> &UiState {
-        &self.ui_state
-    }
+
     fn start_work(&mut self, work: Box<dyn Work + Send>) {
         if let Ok(mut work_handler) = self.work_handler.lock() {
             work_handler.spawn_worker(work);
