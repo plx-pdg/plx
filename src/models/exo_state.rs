467df476ec5400ae83bfc0de96f3eef9fff5315f
André <andre_miguel_costa@hotmail.com>
2024-09-02 12:48:12 +0200

    feat: impl Default for ExoState

--- src/models/exo_state.rs
+++ src/models/exo_state.rs
@@ -6,3 +6,8 @@ pub enum ExoState {
     InProgress, // at least one successful check but not all of them
     Done,       // all checks are successful
 }
+impl Default for ExoState {
+    fn default() -> Self {
+        ExoState::Todo
+    }
+}
