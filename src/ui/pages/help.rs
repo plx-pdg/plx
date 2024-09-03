4095ebd9f268507ebae6e77997be51aa9402cb61
GLK <114987519+G-LK@users.noreply.github.com>
2024-09-03 14:47:29 +0200

    feat: using ratatui tables rather than a list to display keys, descriptions and alts

--- src/ui/pages/help.rs
+++ src/ui/pages/help.rs
@@ -1,9 +1,6 @@
+
 use ratatui::{
-    layout::{Constraint, Direction, Layout},
-    style::{Color, Style},
-    text::Text,
-    widgets::{Block, Borders, List, ListState, Table},
-    Frame,
+    layout::{Constraint, Direction, Layout}, style::{Color, Style}, text::Text, widgets::{Block, Borders, Row, Table}, Frame
 };
 use strum::IntoEnumIterator;
 
@@ -23,35 +20,33 @@ pub fn render_help(frame: &mut Frame) {
     // Render the block with borders
     frame.render_widget(block, layout[0]);
 
-    // Collect all items into a single list
-    let mut items = Vec::new();
+    // USING TABLE
+    let mut rows = Vec::new();
     for k in Key::iter() {
         let key = Text::from(format!("{:?}", k)).style(Style::default().fg(Color::Green));
 
         let description =
-            Text::from(format!("{:?}", k.describe())).style(Style::default().fg(Color::Blue));
+            Text::from(format!("{:?}", k.describe())).style(Style::default().fg(Color::White));  
 
-        let alt = Text::from(format!("{:?}", k.alt()));
+        let alt = Text::from(format!("{:?}", k.alt())).style(Style::default().fg(Color::Blue));
 
-        // Combine all 3 items into a single line
-        // if alt is different from empty string then add it to the line
+        let row = Row::new(vec![key, description, alt]);
 
-        if alt != Text::from("") {
-            let combined_text = format!("{} {} (alt: {})", key, description, alt);
-            items.push(combined_text);
-        } else {
-            let combined_text = format!("{} {}", key, description);
-            items.push(combined_text);
-        }
+        rows.push(row);
+
     }
 
-    // Create a single list with all items
-    let list = List::new(items).block(
-        Block::default()
-            .borders(Borders::ALL)
-            .title("Key Descriptions"),
-    );
+    // Create a table with 3 columns
+    let table = Table::new(rows, vec![
+        Constraint::Percentage(33),
+        Constraint::Percentage(34),
+        Constraint::Percentage(33),
+    ])
+    .block(Block::default().borders(Borders::ALL).title("Key Descriptions"))
+    .widths(&[Constraint::Percentage(33), Constraint::Percentage(34), Constraint::Percentage(33)]); 
 
-    // Render the list inside the inner layout
-    frame.render_widget(list, layout[0]);
+
+    frame.render_widget(table, layout[0]);
+
 }
+
