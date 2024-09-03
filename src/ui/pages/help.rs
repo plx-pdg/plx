f14b221038568e508c05a1747c9675d13c77eaa5
GLK <114987519+G-LK@users.noreply.github.com>
2024-09-03 10:43:42 +0200

    feat: display help page in basic view

--- src/ui/pages/help.rs
+++ src/ui/pages/help.rs
@@ -10,10 +10,48 @@ use strum::IntoEnumIterator;
 use crate::models::key::Key;
 
 pub fn render_help(frame: &mut Frame) {
+    // Create a layout with a full screen display
+    let layout = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(1)
+        .constraints([Constraint::Percentage(100)].as_ref())
+        .split(frame.area());
+
+    // Create a block with borders
+    let block = Block::default().borders(Borders::NONE).title("Help");
+
+    // Render the block with borders
+    frame.render_widget(block, layout[0]);
+
+    // Collect all items into a single list
+    let mut items = Vec::new();
     for k in Key::iter() {
-        println!("{}", format!("{:?} {} {}", k, k.describe(), k.alt()));
+        let key = Text::from(format!("{:?}", k)).style(Style::default().fg(Color::Green));
+
+        let description =
+            Text::from(format!("{:?}", k.describe())).style(Style::default().fg(Color::Blue));
+
+        let alt = Text::from(format!("{:?}", k.alt()));
+
+        // Combine all 3 items into a single line
+        // if alt is different from empty string then add it to the line
+
+        if alt != Text::from("") {
+            let combined_text = format!("{} {} (alt: {})", key, description, alt);
+            items.push(combined_text);
+        } else {
+            let combined_text = format!("{} {}", key, description);
+            items.push(combined_text);
+        }
     }
 
-    // Render the help paragraph in the first chunk
-    // frame.render_widget(help_paragraph, chunks[0]);
+    // Create a single list with all items
+    let list = List::new(items).block(
+        Block::default()
+            .borders(Borders::ALL)
+            .title("Key Descriptions"),
+    );
+
+    // Render the list inside the inner layout
+    frame.render_widget(list, layout[0]);
 }
