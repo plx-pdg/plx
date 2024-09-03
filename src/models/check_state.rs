3b65783d926ce388a678faacaaf51d7e780a82f7
André <andre_miguel_costa@hotmail.com>
2024-09-03 13:13:22 +0200

    feat: add check status for check state. probably a rename is due soon

--- src/models/check_state.rs
+++ src/models/check_state.rs
@@ -1,9 +1,27 @@
 use std::sync::Arc;
 
+use crate::core::diff::diff::Diff;
+
 use super::check::Check;
 
 #[derive(Debug, Clone)]
+pub enum CheckStatus {
+    Passed,
+    Failed(String, String, Diff),
+    Checking,
+    Running,
+    Pending,
+}
+#[derive(Debug, Clone)]
 pub struct CheckState {
     pub(crate) check: Arc<Check>,
-    pub(crate) passed: bool,
+    pub(crate) status: CheckStatus,
+}
+impl CheckState {
+    pub(crate) fn new(check: &Check) -> Self {
+        Self {
+            check: Arc::new(check.clone()),
+            status: CheckStatus::Pending,
+        }
+    }
 }
