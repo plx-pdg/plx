10f507d1e5501534fded43c68e11dd687e33e76a
André <andre_miguel_costa@hotmail.com>
2024-09-03 21:40:06 +0200

    docs add exo status report docs

--- src/core/app/exo_status_report.rs
+++ src/core/app/exo_status_report.rs
@@ -4,6 +4,14 @@ use crate::models::{check_state::CheckState, exo::Exo};
 
 use super::exo_check_result::ExoCheckResult;
 
+///
+/// ExoStatusReport
+///
+/// This struct is used to store the result of a run + check
+/// It keeps the information of an exo run, including the check results,
+/// the compilation output and the path to the elf file
+/// See `ExoCheckResult` for more information about the check results
+///
 pub(super) struct ExoStatusReport {
     pub(super) check_results: Vec<ExoCheckResult>,
     pub(super) compilation_output: Vec<String>,
@@ -12,6 +20,7 @@ pub(super) struct ExoStatusReport {
 }
 
 impl ExoStatusReport {
+    /// Create an ExoStatusReport from an Exo and the target path
     pub(super) fn new(exo: &Exo, elf_path: PathBuf) -> Self {
         let checkers: Vec<ExoCheckResult> = exo
             .checks
@@ -27,6 +36,9 @@ impl ExoStatusReport {
         }
     }
 
+    /// Helper function to get a Vec<CheckState> from check results
+    /// Useful to send the check states to the Ui
+    /// Check `UiState::CheckResults` for more information
     pub(super) fn to_vec_check_state(&self) -> Vec<CheckState> {
         self.check_results
             .iter()
