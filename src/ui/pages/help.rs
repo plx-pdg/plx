4abc57e3f6c4666f29bace9224b2ab76d4dbe10d
GLK <114987519+G-LK@users.noreply.github.com>
2024-09-03 15:45:02 +0200

    refactor: changed display and colors

--- src/ui/pages/help.rs
+++ src/ui/pages/help.rs
@@ -1,6 +1,9 @@
-
 use ratatui::{
-    layout::{Constraint, Direction, Layout}, style::{Color, Style}, text::Text, widgets::{Block, Borders, Row, Table}, Frame
+    layout::{Constraint, Direction, Layout},
+    style::{Color, Style, Stylize},
+    text::Text,
+    widgets::{Block, Borders, Row, Table},
+    Frame,
 };
 use strum::IntoEnumIterator;
 
@@ -23,30 +26,35 @@ pub fn render_help(frame: &mut Frame) {
     // USING TABLE
     let mut rows = Vec::new();
     for k in Key::iter() {
-        let key = Text::from(format!("{:?}", k)).style(Style::default().fg(Color::Green));
+        let key = Text::from(format!("{:#?}", k))
+            .style(Style::default().fg(Color::Green))
+            .alignment(ratatui::layout::Alignment::Left);
 
-        let description =
-            Text::from(format!("{:?}", k.describe())).style(Style::default().fg(Color::White));  
+        let description = Text::from(format!("{:?}", k.describe()))
+            .style(Style::default().fg(Color::White))
+            .centered();
 
-        let alt = Text::from(format!("{:?}", k.alt())).style(Style::default().fg(Color::Blue));
+        let alt = Text::from(format!("{:?}", k.alt()))
+            .style(Style::default().fg(Color::Blue))
+            .alignment(ratatui::layout::Alignment::Right);
 
         let row = Row::new(vec![key, description, alt]);
-
         rows.push(row);
 
     }
 
     // Create a table with 3 columns
-    let table = Table::new(rows, vec![
-        Constraint::Percentage(33),
-        Constraint::Percentage(34),
-        Constraint::Percentage(33),
-    ])
-    .block(Block::default().borders(Borders::ALL).title("Key Descriptions"))
-    .widths(&[Constraint::Percentage(33), Constraint::Percentage(34), Constraint::Percentage(33)]); 
-
+    let table = Table::new(rows, vec![Constraint::Percentage(100)])
+        .block(
+            Block::default()
+                .borders(Borders::ALL)
+                .title("Key Descriptions"),
+        )
+        .widths(&[
+            Constraint::Percentage(33),
+            Constraint::Percentage(34),
+            Constraint::Percentage(33),
+        ]);
 
     frame.render_widget(table, layout[0]);
-
 }
-
