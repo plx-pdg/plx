c5156637899fa5e9d70c4d3e487ba9e2932130ed
André <andre_miguel_costa@hotmail.com>
2024-09-03 11:42:35 +0200

    feat: add on check and on process event handlers

--- src/core/app/event_handler.rs
+++ src/core/app/event_handler.rs
@@ -17,4 +17,26 @@ impl App {
             Key::Esc => todo!(),
         }
     }
+    pub(super) fn on_process_creation_fail(&mut self) {
+        //TODO warn user
+    }
+    pub(super) fn on_process_output_line(&mut self, run_idx: usize, line: String) {
+        if let Some(ref mut cr) = self.current_run {
+            // cr.output_lines[run_idx].push(line);
+        }
+    }
+    fn on_check_status(&mut self, check_idx: usize, passed: bool) {
+        if let Some(ref mut cr) = self.current_run {
+            if check_idx < cr.checks.len() {
+                cr.checks[check_idx].passed = passed;
+            }
+        }
+    }
+    pub(super) fn on_check_passed(&mut self, check_idx: usize) {
+        self.on_check_status(check_idx, true);
+    }
+
+    pub(super) fn on_check_failed(&mut self, check_idx: usize) {
+        self.on_check_status(check_idx, false);
+    }
 }
