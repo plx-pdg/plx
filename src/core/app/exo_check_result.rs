3569799d2e52c1ac926dd328f4a8a4c2e7bf6ec2
André <andre_miguel_costa@hotmail.com>
2024-09-03 21:40:14 +0200

    docs: add exo check result docs

--- src/core/app/exo_check_result.rs
+++ src/core/app/exo_check_result.rs
@@ -1,11 +1,19 @@
 use crate::models::{check::Check, check_state::CheckState};
 
+///
+/// ExoCheckResult
+///
+/// This struct is used to store the result of a run + check
+/// Each exo run will have as many ExoCheckResults as the number of checks the exo has
+/// This helps us keep the output of the run and the check state together
+///
 pub(super) struct ExoCheckResult {
     pub(super) state: CheckState,
     pub(super) output: Vec<String>,
 }
 
 impl ExoCheckResult {
+    /// Create an ExoCheckResult from a Check
     pub(super) fn new(check: &Check) -> Self {
         Self {
             state: CheckState::new(check),
