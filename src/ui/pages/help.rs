193ddc36093181e36c37f1c4b79fc7158fd464ed
Samuel Roland <samuelroland@noreply.codeberg.org>
2024-09-03 23:44:55 +0200

    refactor(help): remove unused layout and show shortcut and alt nicely

--- src/ui/pages/help.rs
+++ src/ui/pages/help.rs
@@ -1,8 +1,8 @@
 use ratatui::{
-    layout::{Constraint, Direction, Layout},
-    style::{Color, Style, Styled, Stylize},
-    text::Text,
-    widgets::{Block, Borders, Row, Table},
+    layout::Constraint,
+    style::{Color, Style, Stylize},
+    text::{Line, Span},
+    widgets::{Block, Row, Table},
     Frame,
 };
 use strum::IntoEnumIterator;
@@ -10,45 +10,41 @@ use strum::IntoEnumIterator;
 use crate::models::key::Key;
 
 pub fn render_help(frame: &mut Frame) {
-    // Create a layout with a full screen display
-    let layout = Layout::default()
-        .direction(Direction::Vertical)
-        .margin(1)
-        .constraints([Constraint::Percentage(100)].as_ref())
-        .split(frame.area());
-
-    // Create a block with borders
-    let block = Block::default().borders(Borders::NONE).title("Help");
-
-    // Render the block with borders
-    frame.render_widget(block, layout[0]);
-
     // USING TABLE
     let mut rows = Vec::new();
+    let mut max_shortcut_text_length: u16 = 0;
     for k in Key::iter() {
+        let description = Line::from(format!("{}", k.describe()))
+            .style(Style::default().fg(Color::White))
+            .dim();
 
-        let description = Text::from(format!("{}", k.describe()))
-            .style(Style::default().fg(Color::White)).dim();
+        let mut shortcut_text = Line::default();
+        let shortcut_name = format!("{:?}", k);
+        shortcut_text.push_span(Span::from(shortcut_name.to_lowercase()).green());
+        let mut length_count: u16 = shortcut_name.len() as u16;
+        if !k.alt().is_empty() {
+            shortcut_text.push_span(Span::from(", ").dim());
+            shortcut_text.push_span(Span::from(k.alt().to_lowercase()).green());
+            length_count += k.alt().len() as u16 + 2;
+        }
 
-        let key_and_alt = Text::from(format!("{:?}, {}", k, k.alt()).to_lowercase()).style(Style::default().fg(Color::Green));
+        if length_count > max_shortcut_text_length {
+            max_shortcut_text_length = length_count;
+        }
 
-        let row = Row::new(vec![key_and_alt, description]);
+        let row = Row::new(vec![Line::from(shortcut_text), description]);
         rows.push(row);
-
     }
-        
-    // 2 columns table | shortcut, alt | description |
-    // Create a table with 2 columns
+
+    //TODO: skip first rows to respect given offset to be able to scroll down
+
+    // Create a table with 2 columns: | shortcut, alt | description |
     let table = Table::new(rows, vec![Constraint::Percentage(100)])
-        .block(
-            Block::default()
-                .borders(Borders::ALL)
-                .title("Key Descriptions"),
-        )
+        .block(Block::default().title("Help of shortcuts"))
         .widths(&[
-            Constraint::Percentage(10),
+            Constraint::Min(max_shortcut_text_length),
             Constraint::Percentage(90),
         ]);
 
-    frame.render_widget(table, layout[0]);
+    frame.render_widget(table, frame.area());
 }
