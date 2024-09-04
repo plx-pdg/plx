b8877f00cabe77594e61c48c100bdb132385e41b
kevin <kevin.auberson@heig-vd.ch>
2024-09-04 17:20:56 +0200

    feat: create log file and write log into file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use simplelog::*;
+use std::fs::File;
 use crate::core::app::app::App;
 pub mod core;
 pub mod models;
 pub mod ui;
 
 fn main() {
+    WriteLogger::init(LevelFilter::Trace, Config::default(), File::create("debug.log").expect("Failed to create log file")).expect("Failed to initialize WriteLogger");
     match App::new() {
         Ok(app) => app.run_forever(),
         Err(err) => {
