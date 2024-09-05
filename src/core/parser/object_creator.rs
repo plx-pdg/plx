3c849844513d64ab9e60603d35512ea22c3bf7d9
André <andre_miguel_costa@hotmail.com>
2024-09-05 19:22:49 +0200

    feat: add write object to file fn

--- src/core/parser/object_creator.rs
+++ src/core/parser/object_creator.rs
@@ -1,6 +1,11 @@
-use serde::de::DeserializeOwned;
+use std::io;
 
-use crate::core::file_utils::{file_parser::ParseError, file_utils::read_file};
+use serde::{de::DeserializeOwned, Serialize};
+
+use crate::core::file_utils::{
+    file_parser::{ParseError, SerializeError},
+    file_utils::{read_file, write_file},
+};
 
 use super::toml_parser;
 
@@ -12,4 +17,14 @@ where
     let me = toml_parser::read_from_toml(&file_content)
         .map_err(|err| ParseError::ParseError(err.to_string()))?;
     Ok(me)
+}
+
+pub fn write_object_to_file<T>(path: &std::path::PathBuf, object: T) -> Result<(), SerializeError>
+where
+    T: Serialize,
+{
+    let repr = toml_parser::toml_serialize(&object)
+        .map_err(|err| SerializeError::SerializeError(err.to_string()))?;
+    write_file(path, &repr).map_err(|err| SerializeError::WriteFileError(err.to_string()))?;
+    Ok(())
 }
