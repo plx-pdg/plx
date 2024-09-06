a931c56ab7a631e888f6b07dbb2098ef1e8bab26
André <andre_miguel_costa@hotmail.com>
2024-09-06 21:36:13 +0200

    docs: process handler comments

--- src/core/process/process_handler.rs
+++ src/core/process/process_handler.rs
@@ -20,6 +20,9 @@ pub enum ProcessStatus {
     Done(ExitStatus),
     Running,
 }
+/// Launches a sub process `cmd` using `args`
+/// Stdout and stderr are piped and can then be retrieved using the Child returned
+/// eg: child.stdout.take() and child.stderr.take()
 pub fn spawn_process(cmd: &str, args: Vec<String>) -> Result<Child, ProcessError> {
     let child = Command::new(OsStr::new(&cmd))
         .args(args)
@@ -30,6 +33,11 @@ pub fn spawn_process(cmd: &str, args: Vec<String>) -> Result<Child, ProcessError
 
     Ok(child)
 }
+/// Polls the process status without blocking
+/// Can be called in a loop as it will handling sleep so it doesn't use 100% of the cpu
+/// This function may never return ProcessStatus::Done if it blocks waiting for stdin
+/// In order to make sure we can actually kill a process that is blocked waiting for stdin,
+/// call `stop_child` and then `capture_exit_status`
 pub fn get_process_status(child: &mut Child) -> Result<ProcessStatus, ProcessError> {
     match child.try_wait() {
         Ok(status) => match status {
@@ -42,12 +50,22 @@ pub fn get_process_status(child: &mut Child) -> Result<ProcessStatus, ProcessErr
         Err(_) => return Err(ProcessError::WaitChildFail),
     }
 }
+
+/// Kills Child
+/// /!\ Currently some weird issues happen on windows where the child is not really killed
+/// depending on the situation /!\
 pub fn stop_child(child: &mut Child) -> Result<(), io::Error> {
     child.kill()
 }
+/// Captures the child exit status
+/// This function will wait for the child process and drop the stdin pipe so if the child process
+/// is blocked waiting for stdin, this will unblock it
 pub fn capture_exit_status(child: &mut Child) -> Result<ExitStatus, io::Error> {
     child.wait()
 }
+
+/// Waits for the child process to end
+/// While waiting, setting `should_stop` to true will kill the process
 pub fn wait_child(
     child: &mut Child,
     should_stop: Arc<AtomicBool>,
