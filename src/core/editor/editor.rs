ebdb3f2ac03a192fe030ae0f9b5de62bb1602d5e
André <andre_miguel_costa@hotmail.com>
2024-09-06 22:07:36 +0200

    feat: make sure we don't try to launch an invalid editor (workaround for now)

--- src/core/editor/editor.rs
+++ src/core/editor/editor.rs
@@ -2,7 +2,18 @@ use std::env;
 
 pub fn get_default_editor() -> Option<String> {
     match env::var("EDITOR") {
-        Ok(editor) => Some(editor.into()),
+        Ok(editor) => {
+            let valid_editors: Vec<String> = vec![
+                String::from("code"),
+                String::from("codium"),
+                String::from("idea"),
+            ];
+            if valid_editors.contains(&editor) {
+                Some(editor)
+            } else {
+                None
+            }
+        }
         Err(_) => None,
     }
 }
